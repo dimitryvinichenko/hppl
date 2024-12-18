ccfaf463ef260e9e4831ebf94a76e0b6318514d3
Dmitry Vinichenko <133007241+dimitryvinichenko@users.noreply.github.com>
2024-12-18 08:21:29 +0300

    Update heatmap.py

--- heatmap.py
+++ heatmap.py
@@ -4,8 +4,6 @@ import matplotlib.animation as animation
 from matplotlib.animation import FuncAnimation
 
 def heatmap(grid, iteration, ghostcells, im_dir, number_processes):
-    #plot heatmap using seaborn
-    #andere farbe YlGnBu /coolwarm
     sb.heatmap(grid, cmap="YlGnBu")
     plt.title("{} Iterationenen".format((iteration + 1)*ghostcells))
     plt.xlabel("x")
